be751c91fd22fcfb267428856fd80af13f75ac01
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-01-21 14:13:11 +0700

    connection on mongoDb

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,15 @@ import cors from "cors";
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/fullstack_db", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Database Connected..."));
+
+app.use(cors());
+app.use(express.json());
+
 app.listen(5000, () => console.log("Server up and running..."));
