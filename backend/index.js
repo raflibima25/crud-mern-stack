ccf8749ebbbac1b5b5a14e0574b61ec0ef4c60dc
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-01-24 10:07:35 +0700

    add connection to mongoDB

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ mongoose.connect("mongodb://localhost:27017/fullstack_db", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Database Connected..."));
